6347231517074deceaa63c67c5bf13ea2942cba4
AndreaScacchi <andreascacchi10@gmail.com>
2022-07-25 15:40:52 +0200

    printBoard function implemented and declared inside the main funciton. .gitignore file generated

--- game.c
+++ game.c
@@ -24,6 +24,7 @@ void printWinner(char);
 int main() {
     char winner = ' ';
     resetBoard();
+    printBoard();
 
     return 0;
 }
@@ -39,7 +40,12 @@ void resetBoard() {
 }
 
 void printBoard() {
-
+    printf(" %c | %c | %c ", board[0][0], board[0][1], board[0][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ", board[2][0], board[2][1], board[2][2]);
+    printf("\n");
 }
 
 int checkFreeSpaces() {
