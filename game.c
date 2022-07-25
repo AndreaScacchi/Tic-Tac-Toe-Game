97b626523fd8ca80c532161dcae5a33b2ed263ea
AndreaScacchi <andreascacchi10@gmail.com>
2022-07-25 16:43:06 +0200

    checkWinner function implemented

--- game.c
+++ game.c
@@ -28,6 +28,7 @@ int main() {
     while(winner == ' ' && checkFreeSpaces() != 0) {
         printBoard();
         playerMove();
+        winner = checkWinner();
     }
 
     return 0;
@@ -93,7 +94,29 @@ void computerMove() {
 }
 
 char checkWinner() {
+    // Check rows
+    for(int i = 0; i < 3; i++) {
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+            return board[i][0];
+        }
+    }
 
+    // Check columns
+    for(int i = 0; i < 3; i++) {
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+            return board[0][i];
+        }
+    }
+
+    // Check diagonals
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+            return board[0][0];
+        }
+    if(board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+            return board[0][2];
+        }
+
+    return ' ';
 }
 
 void printWinner(char winner) {
