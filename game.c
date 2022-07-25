eb6fb936270a6823a9e0f564da2bd67452c298b4
AndreaScacchi <andreascacchi10@gmail.com>
2022-07-25 17:03:32 +0200

    game finished

--- game.c
+++ game.c
@@ -23,25 +23,37 @@ void printWinner(char);
 // Main function
 int main() {
     char winner = ' ';
-    resetBoard();
+    char response;
+    do
+    {
+        winner = ' ';
+        response = ' ';
+        resetBoard();
 
-    while(winner == ' ' && checkFreeSpaces() != 0) {
+        while(winner == ' ' && checkFreeSpaces() != 0) {
+            printBoard();
+            playerMove();
+            winner = checkWinner();
+            if(winner != ' ' || checkFreeSpaces() == 0) {
+                break;
+            }
+
+            computerMove();
+            winner = checkWinner();
+            if(winner != ' ' || checkFreeSpaces() == 0) {
+                break;
+            }
+        }
+
         printBoard();
-        playerMove();
-        winner = checkWinner();
-        if(winner != ' ' || checkFreeSpaces() == 0) {
-            break;
-        }
-
-        computerMove();
-        winner = checkWinner();
-        if(winner != ' ' || checkFreeSpaces() == 0) {
-            break;
-        }
-    }
-
-    printBoard();
-    printWinner(winner);
+        printWinner(winner);
+        printf("\nWould you like to play again? (Y/N): ");
+        scanf("%c");
+        scanf("%c", &response);
+        response = toupper(response);
+    } while (response == 'Y');
+    
+    printf("Thanks for playing!");
 
     return 0;
 }
